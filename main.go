6a557019dd20fbf3f9b5991d9febe11fe65865fa
H4XOR_ <hakroot07@gmail.com>
2024-08-05 18:17:27 +0200

    Function usage 'LoadQuestions' in main.go

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
+	"quiz/questions"
+)
 
+func main() {
+	questions, err := questions.LoadQuestions()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Couldn't load questions: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Println(questions)
 }
